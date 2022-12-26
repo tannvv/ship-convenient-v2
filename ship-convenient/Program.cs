9886c5e2cec4920859136fe38f6417ea9f8a15ea
tannvv <nvtan.a5@gmail.com>
2022-12-26 18:09:00 +0700

    enable swagger

--- ship-convenient/Program.cs
+++ ship-convenient/Program.cs
@@ -25,12 +25,8 @@ builder.Services.AddSwaggerApp();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
