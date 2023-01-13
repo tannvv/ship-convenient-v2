4436fe91a00594fc0600df0f02cea360acb13f16
tannvv <nvtan.a5@gmail.com>
2023-01-13 10:37:27 +0700

    fix bug empty gender

--- ship-convenient/Model/UserModel/ResponseInfoUserModel.cs
+++ ship-convenient/Model/UserModel/ResponseInfoUserModel.cs
@@ -8,6 +8,7 @@ namespace ship_convenient.Model.UserModel
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string PhotoUrl { get; set; } = string.Empty;
         public List<ResponseRouteModel> Routes { get; set; } = new();
