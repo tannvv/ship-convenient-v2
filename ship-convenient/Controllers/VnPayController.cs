bb563f53865dc09b8a6f36e045fe69faa446bfd5
tannvv <nvtan.a5@gmail.com>
2023-02-01 09:54:46 +0700

    update result vnpay

--- ship-convenient/Controllers/VnPayController.cs
+++ ship-convenient/Controllers/VnPayController.cs
@@ -34,7 +34,7 @@ namespace ship_convenient.Controllers
         [HttpGet]
         public async Task<IActionResult> Get([FromQuery] PaymentVnPayModel model)
         {
-            string returnUrl = _configuration["VnPay:ReturnAdminPath"];
+            string returnUrl = _configuration["VnPay:ReturnPathResult"];
             string tmnCode = _configuration["VnPay:TmnCode"];
 
             _vnPayService.AddRequest("vnp_Version", "2.1.0"); //Phiên bản api mà merchant kết nối. Phiên bản hiện tại là 2.0.0
