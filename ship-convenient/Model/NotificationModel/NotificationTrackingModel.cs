975eeab7b984df20448a389546a1ec78d66341f9
tannvv <nvtan.a5@gmail.com>
2023-02-09 22:34:21 +0700

    update key

--- ship-convenient/Model/NotificationModel/NotificationTrackingModel.cs
+++ ship-convenient/Model/NotificationModel/NotificationTrackingModel.cs
@@ -3,7 +3,7 @@
     public class NotificationTrackingModel
     {
         public Guid DeliverId { get; set; }
-        public string Title { get; set; } = string.Empty;
+        public string TypeOfNotification { get; set; } = string.Empty;
         public IReadOnlyDictionary<string, string> Data { get; set; } = new Dictionary<string, string>();
     }
 }
