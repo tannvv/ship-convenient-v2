20a48b6473a2ec4321f61991a9d2d261851f4716
tannvv <nvtan.a5@gmail.com>
2023-03-16 18:21:45 +0700

    change api

--- ship-convenient/Config/FirebaseExtension.cs
+++ ship-convenient/Config/FirebaseExtension.cs
@@ -7,7 +7,8 @@ namespace ship_convenient.Config
     {
         public static void AddFirebaseApp(this IServiceCollection services) {
             FirebaseApp.Create(new AppOptions()
-            {
+            { 
+
                 Credential = GoogleCredential.FromFile("Config/Firebase/convenient-way-firebase-adminsdk-t8g11-38ee5771a0.json")
             });
         }
